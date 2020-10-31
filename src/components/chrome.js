f23c33aa38b64f7443f3400503235285e574c7b1
badhon kundu <badhonkundu@gmail.com>
2020-10-31 14:33:43 +0530

    added routing boilerplate

--- src/components/chrome.js
+++ src/components/chrome.js
@@ -1,13 +1,20 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import Login from './login';
 import Workflow from './workflow';
 import Workflows from './workflows';
+import NotFound from './notFound';
+
 
 function Chrome(props) {
   return (
     <div>
-      <Login />
-      <Workflow />
-      <Workflows />
+      <Switch>
+        <Route path="/" exact component={Workflows} />
+        <Route path="/login" component={Login} />
+        <Route path="/:id" render={Workflow} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/notFound.js
@@ -0,0 +1,9 @@
+function NotFound(props) {
+  return (
+    <div>
+      The page you are looking for does not exist
+    </div>
+  );
+}
+
+export default NotFound;
